bae3ccbd6f396d7c72ec824d81d0da7e98ab8aaf
Vladimir Alekseev <vovauu@icloud.com>
2023-01-29 01:49:35 +0300

    add basic code and design

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    let eggTimes = ["Soft":3.0, "Medium":5.0, "Hard":7.0]
+    
+    var timer = Timer()
+    
+    var totalTime = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        totalTime = eggTimes[sender.currentTitle!]!
+        timer.invalidate()
+        
+        progressBar.progress = 0.0
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:  #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    }
+    @objc func updateCounter() {
+        if progressBar.progress < 1.0 {
+            progressBar.progress += (1.0/Float(totalTime))
+            print("\(progressBar.progress)")
+            
+        } else {
+            questionLabel.text = "Done!"
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                self.questionLabel.text = "How do you like your eggs?"
+                
+            }
+            timer.invalidate()
+            
+        }
+        
+    }
 }
+
 
